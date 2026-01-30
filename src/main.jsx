1c53a1bfae04a374bffdf3526b3330198dd7e434
Rudra Pandya <viralpandya079@gmail.com>
2026-01-31 01:52:14 +0530

    Fix React 17 render API for Netlify build

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <App />
-);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
